d7a4c3f3f1f5b85810d6da4bdd4b7d07223780a3
gamerstt <32896049+gamerstt@users.noreply.github.com>
2017-10-18 14:41:18 +0530

    Update server.go

--- server/server.go
+++ server/server.go
@@ -16,8 +16,8 @@ import (
 	"time"
 
 	"github.com/NYTimes/gziphandler"
-	"github.com/jpillora/cloud-torrent/engine"
-	"github.com/jpillora/cloud-torrent/static"
+	"github.com/gamerstt/Actsg/engine"
+	"github.com/gamerstt/Actsg/static"
 	"github.com/jpillora/cookieauth"
 	"github.com/jpillora/requestlog"
 	"github.com/jpillora/scraper/scraper"
